97d93862364af42194461b3649546446c7daa95d
Jannik Brozy <StyXinator98@googlemail.com>
2024-11-28 15:40:23 +0100

    Add files via upload

---
+++ Labor3.cpp
@@ -0,0 +1,9 @@
+// Labor3.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
